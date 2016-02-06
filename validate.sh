13e595ca6486cf3bd33313b1e7a756b1e6f83f56
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-02-05 19:37:01 -0500

    Updated JSON-java and schema

--- validate.sh
+++ validate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 javac -classpath json-schema/core/target/org.everit.json.schema-1.1.2-SNAPSHOT.jar:. *.java
-node validatex3djson.js CC.json
-java -classpath json-schema/core/target/org.everit.json.schema-1.1.2-SNAPSHOT.jar:. ObjectTest CC.json
+#node validatex3djson.js CC.json
+#java -classpath json-schema/core/target/org.everit.json.schema-1.1.2-SNAPSHOT.jar:. ObjectTest CC.json
 java -classpath json-schema/core/target/org.everit.json.schema-1.1.2-SNAPSHOT.jar:. MetaSchemaTest
 find examples/ -name '*.json' -print0 |xargs -0 java -classpath json-schema/core/target/org.everit.json.schema-1.1.2-SNAPSHOT.jar:. ObjectTest | tee javaresults.txt
-grep -w Validation javaresults.txt|sort |uniq -c|sort -nr > ordered.txt
+grep -w Validation javaresults.txt| sed 's/[^:]*://'| sort |uniq -c|sort -nr > ordered.txt
 find examples/ -name '*.json' -print0 |xargs -0 node validatex3djson.js > results.txt
