8ca4fe7434eb0b399d07fca0f34e03f402247934
John Carlson <yottzumm@gmail.com>
2017-02-22 18:27:56 -0500

    new schema, reworked validate.sh

--- validate.sh
+++ validate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
-find /www.web3d.org/x3d/content/examples/ -name '*.json' -print0 |xargs -0 node validatex3djson.js | tee results.txt
-javac -classpath json-schema/core/target/org.everit.json.schema-1.2.0.jar:. *.java
+export CLASSPATH=".;C:/Users/coderextreme/.m2/repository/com/google/collections/google-collections/1.0/google-collections-1.0.jar;C:/Users/coderextreme/.m2/repository/org/json/json/20160810/json-20160810.jar;json-schema/core/target/org.everit.json.schema-1.5.0.jar"
+find ../X3DJSONLD/www.web3d.org/x3d/content/examples/ -name '*.json' -print0 |xargs -0 node validatex3djson.js | tee results.txt
+javac -classpath $CLASSPATH *.java
 #node validatex3djson.js CC.json
-#java -classpath json-schema/core/target/org.everit.json.schema-1.2.0.jar:. ObjectTest CC.json
-java -classpath json-schema/core/target/org.everit.json.schema-1.2.0.jar:. MetaSchemaTest
-find /www.web3d.org/x3d/content/examples/ -name '*.json' -print0 |xargs -0 java -classpath json-schema/core/target/org.everit.json.schema-1.2.0.jar:. ObjectTest | tee javaresults.txt
-grep -w Validation javaresults.txt| sed 's/[^:]*://'| sed 's/\/www.web3d.org\/x3d\/content\/examples\/\/.*//'| sort |uniq -c|sort -nr > ordered.txt
+#java -classpath $CLASSPATH ObjectTest CC.json
+java -classpath $CLASSPATH MetaSchemaTest
+find ../X3DJSONLD/www.web3d.org/x3d/content/examples/ -name '*.json' -print0 |xargs -0 java -classpath $CLASSPATH ObjectTest | tee javaresults.txt
+grep -w Validation javaresults.txt| sed 's/[^:]*://'| sed 's/..\/X3DJSONLD\/www.web3d.org\/x3d\/content\/examples\/\/.*//'| sort |uniq -c|sort -nr > ordered.txt
 egrep 'BEGIN|json-parse' javaresults.txt > jsonparseresults.txt
