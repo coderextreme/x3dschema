c35b6ada537e4e512fc07bf72904c5ea7ef7147f
John Carlson <yottzumm@gmail.com>
2018-04-16 04:01:23 -0400

    fixed validateone.sh

--- validateone.sh
+++ validateone.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
-javac -classpath json-schema/core/target/org.everit.json.schema-1.2.0.jar:. *.java
-java -classpath json-schema/core/target/org.everit.json.schema-1.2.0.jar:. ObjectTest "$@"
+export CLASSPATH=".;C:/Users/${USERNAME}/.m2/repository/com/google/collections/google-collections/1.0/google-collections-1.0.jar;C:/Users/${USERNAME}/.m2/repository/org/json/json/20180130/json-20180130.jar;C:/Users/${USERNAME}/.m2/repository/com/damnhandy/handy-uri-templates/2.1.6/handy-uri-templates-2.1.6.jar;json-schema/core/target/org.everit.json.schema-1.8.0.jar"
+javac *.java
+java ObjectTest "$@"
 node validatex3djson.js "$@"
