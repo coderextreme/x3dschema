1207586265c4f3aadd01d9747b9af36ed6ba36c2
John Carlson <yottzumm@gmail.com>
2018-10-22 18:47:05 -0500

    check in results

--- validate.sh
+++ validate.sh
@@ -3,7 +3,7 @@ git clone https://github.com/everit-org/json-schema
 pushd json-schema
 ~/apache-maven-3.5.2/bin/mvn -Dmaven.test.skip=true install
 popd
-export CLASSPATH=".;C:/Users/${USERNAME}/.m2/repository/com/google/collections/google-collections/1.0/google-collections-1.0.jar;C:/Users/${USERNAME}/.m2/repository/org/json/json/20180130/json-20180130.jar;C:/Users/${USERNAME}/.m2/repository/com/damnhandy/handy-uri-templates/2.1.6/handy-uri-templates-2.1.6.jar;json-schema/core/target/org.everit.json.schema-1.8.0.jar"
+export CLASSPATH=".;C:/Users/${USERNAME}/.m2/repository/com/google/re2j/re2j/1.1/re2j-1.1.jar;C:/Users/${USERNAME}/.m2/repository/com/google/collections/google-collections/1.0/google-collections-1.0.jar;C:/Users/${USERNAME}/.m2/repository/org/json/json/20180130/json-20180130.jar;C:/Users/${USERNAME}/.m2/repository/com/damnhandy/handy-uri-templates/2.1.6/handy-uri-templates-2.1.6.jar;json-schema/core/target/org.everit.json.schema-1.8.0.jar"
 export WEB3D=/c/x3d-code/www.web3d.org
 javac -classpath $CLASSPATH *.java
 java -classpath $CLASSPATH MetaSchemaTest
